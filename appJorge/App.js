d36a67ba6efcb1c764b5549d2ad86c3decdb7b29
Jorge Vidales <jorgev_328@hotmail.com>
2020-09-01 18:50:07 -0500

    subi fuentes

--- appJorge/App.js
+++ appJorge/App.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react';
 import {AppRegistry, StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { white } from 'chalk';
 
 export default class DisplayAnImage extends Component {
   render() {
     const image = 'https://freesvg.org/img/david-bowie-maze-online-casino-real-money-art.png'
-    const name = 'David Bowin 3'
+    const name = 'David Bowin'
+    const likes = 200
+    const comments = 390
     const onPress = () => console.info('Prueba 2');
     return (
       
       
       <View  style={styles.container} >
-        <TouchableOpacity onPress={onPress}>
+        
         <View style={styles.artisBox}>
           <Image style={styles.image} source={{ uri: image}}/>
           <View style={styles.info}>
             <Text style={styles.name}>{name}</Text>
+            <View style={styles.row}>
+              <View style={styles.iconContainer}>
+                <TouchableOpacity onPress={onPress}>
+                  <Icon size={30}  color='gray' name='heart-outline'></Icon>
+                  <Text style={styles.count}>{likes}</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.iconContainer}>
+                <TouchableOpacity onPress={onPress}>
+                  <Icon size={30}  color='gray' name='chat-processing-outline'></Icon>
+                  <Text style={styles.count}>{comments}</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
+
         </View>
-        </TouchableOpacity>
       </View>
       
     );
@@ -40,12 +58,27 @@ const styles = StyleSheet.create({
   },
   info:{
     flex: 1,
-    justifyContent: "center",
-    flexDirection: 'row',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   name:{
     fontSize: 20,
     marginTop: 10,
+  },
+  row:{
+    //backgroundColor: 'red',
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    marginHorizontal: 40,
+    marginTop: 10,
+  },
+  iconContainer:{
+    flex: 1,
+    alignItems: 'center',
+  },
+  count:{
+    color: 'gray',
   }
  
 });
